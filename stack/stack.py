59be10c32bfa0a479e43a767e454c9fa20f30a90
tchan102 <tchan102@mail.ccsf.edu>
2022-03-06 00:06:07 -0800

    first commit

---
+++ stack/stack.py
@@ -0,0 +1,23 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    def get_stack(self):
+        return self.items
+
+
+# Stack is in LIFO (Last in First out) structure
+# normally with three methods push peek pop
